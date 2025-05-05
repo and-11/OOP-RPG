646f6b0306d1cc3cd32187606af9df0128170c0a
Turcu Andrei Cristian <andreiturcu2005.24@gmail.com>
2025-05-05 18:19:17 +0300

    one lib

--- libraryes.h
+++ libraryes.h
@@ -13,3 +13,13 @@
 #include <climits>
 
 #include <cstdlib>
+//////////////////
+#include "potion.h"
+#include "functions.h"
+#include "player.h"
+#include "game.h"
+#include "goblin.h"
+#include "item.h"
+#include "entity.h"
+#include "exception.h"
+#include "user_interface.h"
--- main.cpp
+++ main.cpp
@@ -1,31 +1,19 @@
 
-#include "functions.h"
-
-
-
-#include "potion.h"
-
-#include "player.h" 
-#include "game.h" 
-#include "goblin.h" 
-#include "item.h" 
-#include "entity.h" 
-#include "exception.h" 
-#include "functions.h" 
-#include "user_interface.h"
+#include "libraryes.h"
 
 void setup()
 {
-    Game level,lvl2;
+    Game level;
+    // Game lvl2;
     UI jocc;
 
     std::string name = "KING";
     std::shared_ptr<Entity> x = std::make_shared<Player>( 1,10,name,2,2 ) ;
     level.add_creature( x );
 
-    name = "olo";
-    x = std::make_shared<Player>( 1,10,name,2,2 ) ;
-    level.add_creature( x );
+    // name = "olo";
+    // x = std::make_shared<Player>( 1,10,name,2,2 ) ;
+    // level.add_creature( x );
     
     
 
@@ -33,21 +21,21 @@ void setup()
     std::shared_ptr<Item> y = std::make_shared<Potion>( name,-1,25 );
     level.add_item( y );
 
-    name = "Lightning";
-    y = std::make_shared<Potion>( name,0,-100 );
-    level.add_item( y );
+    // name = "Lightning";
+    // y = std::make_shared<Potion>( name,0,-100 );
+    // level.add_item( y );
 
-    name = "Increase_stats";
-    y = std::make_shared<Potion>( name,4,4 );
-    level.add_item( y );
+    // name = "Increase_stats";
+    // y = std::make_shared<Potion>( name,4,4 );
+    // level.add_item( y );
 
 
     x= std::make_shared<Goblin>( 2 );
     level.add_creature( x );
-    x= std::make_shared<Goblin>( 1 );
-    level.add_creature( x );
-    x= std::make_shared<Goblin>( 3 );
-    level.add_creature( x );
+    // x= std::make_shared<Goblin>( 1 );
+    // level.add_creature( x );
+    // x= std::make_shared<Goblin>( 3 );
+    // level.add_creature( x );
 
     jocc.Add_level(level);
     
@@ -59,6 +47,12 @@ void setup()
     // level.add_creature( x );
     
     jocc.start();
+
+    
+    
+    ///     -   --- -   -   -   -   -   -   --  -   -   
+    x.reset();
+    y.reset();
 }
 
 
