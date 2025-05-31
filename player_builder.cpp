7bae7d9ecf708f8aa878d4c007d7b930c364ee9b
Turcu Andrei Cristian <andreiturcu2005.24@gmail.com>
2025-05-31 12:33:12 +0300

    builder is now split into hpp and cpp

--- player_builder.cpp
+++ player_builder.cpp
@@ -1,1 +1,71 @@
 #include "player_builder.hpp"
+
+
+
+Player_Builder::Player_Builder() {
+    player = std::make_shared<Player>();
+}
+[[nodiscard]] std::shared_ptr<Player> Player_Builder::get_player() const {
+    return player;
+}
+
+void Knight_Player_Builder::build_player()  {
+    player->set_player( true );
+
+    player->set_damage(4);
+    player->set_health(10);
+
+    std::string name = "Knight";
+    player->set_name(name);
+
+    player->set_coefficient_damage(2);
+    player->set_coefficient_health(2);
+
+    player->set_level(1);
+}
+
+void Archer_Player_Builder::build_player()  {
+    player->set_player( true );
+
+    player->set_damage(10);
+    player->set_health(6);
+
+    std::string name = "Archer";
+    player->set_name(name);
+
+    player->set_coefficient_damage(2);
+    player->set_coefficient_health(2);
+
+    player->set_level(1);
+}
+
+void Shield_Player_Builder::build_player() {
+    player->set_player( true );
+
+    player->set_damage(1);
+    player->set_health(23);
+
+    std::string name = "Shield";
+    player->set_name(name);
+
+    player->set_coefficient_damage(2);
+    player->set_coefficient_health(2);
+
+    player->set_level(1);
+}
+
+
+
+void Player_Builder_Director::set_player_builder(std::shared_ptr<Player_Builder> player_builder) {
+    this->player_builder = player_builder;
+}
+
+void Player_Builder_Director::build_player() {
+    player_builder->build_player();
+}
+
+std::shared_ptr<Player> Player_Builder_Director::get_player() {
+    return player_builder->get_player();
+}
+
+
--- player_builder.hpp
+++ player_builder.hpp
@@ -8,70 +8,30 @@ class Player_Builder
 protected:
     std::shared_ptr<Player> player;
 public:
-    Player_Builder() {
-        player = std::make_shared<Player>();
-    }
+    Player_Builder();
+
     virtual void build_player() = 0;
 
-    [[nodiscard]] std::shared_ptr<Player> get_player() const {
-        return player;
-    }
+    [[nodiscard]] std::shared_ptr<Player> get_player() const;
+
     virtual ~Player_Builder() = default;
 };
 
 class Knight_Player_Builder : public Player_Builder {
 public:
-    void build_player() override {
-        player->set_player( true );
-
-        player->set_damage(4);
-        player->set_health(10);
-
-        std::string name = "Knight";
-        player->set_name(name);
-
-        player->set_coefficient_damage(2);
-        player->set_coefficient_health(2);
-
-        player->set_level(1);
-    }
+    void build_player() override;
     Knight_Player_Builder() = default;
 };
 class Archer_Player_Builder : public Player_Builder {
 public:
-    void build_player() override {
-        player->set_player( true );
+    void build_player() override ;
 
-        player->set_damage(10);
-        player->set_health(6);
-
-        std::string name = "Archer";
-        player->set_name(name);
-
-        player->set_coefficient_damage(2);
-        player->set_coefficient_health(2);
-
-        player->set_level(1);
-    }
     Archer_Player_Builder() = default;
 };
 
 class Shield_Player_Builder : public Player_Builder {
 public:
-    void build_player() override {
-        player->set_player( true );
-
-        player->set_damage(1);
-        player->set_health(23);
-
-        std::string name = "Shield";
-        player->set_name(name);
-
-        player->set_coefficient_damage(2);
-        player->set_coefficient_health(2);
-
-        player->set_level(1);
-    }
+    void build_player() override ;
     Shield_Player_Builder() = default;
 };
 
@@ -83,15 +43,9 @@ private:
 public:
     Player_Builder_Director() = default;
 
-    void set_player_builder(std::shared_ptr<Player_Builder> player_builder) {
-        this->player_builder = player_builder;
-    }
+    void set_player_builder(std::shared_ptr<Player_Builder> player_builder);
 
-    void build_player() {
-        player_builder->build_player();
-    }
+    void build_player();
 
-    std::shared_ptr<Player> get_player() {
-        return player_builder->get_player();
-    }
+    std::shared_ptr<Player> get_player();
 };
